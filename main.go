992877618e8bc05187564c15d5ec0e84f3053cc8
zmanxiang <zmanxiang@fortinet.com>
2021-04-15 17:56:17 -0700

    ADD: send request

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func main() {
+	for {
+		script := "curl http://test-server:8090"
+		var out bytes.Buffer
+		cmd := exec.Command("/bin/sh")
+		cmd.Stdin = strings.NewReader(script)
+		cmd.Stdout = &out
+		cmd.Stderr = &out
+		errmsgs := ""
+		if err := cmd.Run(); err != nil {
+			errmsgs += fmt.Sprintf("failed to run: %q, output: %q, error: %s\n", script, out.String(), err)
+			log.Fatal(errmsgs)
+		}
+
+		fmt.Println(out.String())
+		time.Sleep(8 * time.Second)
+	}
+
+}
